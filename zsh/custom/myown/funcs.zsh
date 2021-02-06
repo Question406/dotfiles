a109acafe65dc91624934d66556726e415215b91
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-02-06 14:49:04 +0000

    update gitconfig & aliases

--- zsh/custom/myown/funcs.zsh
+++ zsh/custom/myown/funcs.zsh
@@ -1,2 +1,9 @@
 # shell functions
 
+
+# git init a directory and .gitignore template from github, from https://stackoverflow.com/questions/17932658/git-init-with-option-for-auto-creation-of-a-default-gitignore
+git_init_more(){
+    git init .
+    curl -o .gitignore --fail --show-error --silent --location https://raw.github.com/github/gitignore/master/$1.gitignore
+    git add .gitignore && git commit -m "added gitignore from GitHub"
+}
