56bde6005b10affac6aaa7a072d80cd5f50ce93a
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-02-19 11:45:23 +0800

    even ssh is not included in arch...

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm --needed wget
 
 bash scripts/common.sh
-sudo pacman -S --noconfirm --needed zsh vim tmux nodejs
+sudo pacman -S --noconfirm --needed zsh vim tmux nodejs openssh
 chsh -s $(which zsh)
