caa083885d0f7d7cfc976aed2045b2b471571baa
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-02-19 11:32:56 +0800

    script to install nodejs on archlinux

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm --needed wget
 
 bash scripts/common.sh
-sudo pacman -S --noconfirm --needed zsh vim tmux
+sudo pacman -S --noconfirm --needed zsh vim tmux nodejs
 chsh -s $(which zsh)
--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -1,3 +1,3 @@
 bash scripts/common.sh
 
-sudo pacman -S --noconfirm --needed vim tmux
+sudo pacman -S --noconfirm --needed vim tmux nodejs
