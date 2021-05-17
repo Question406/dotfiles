b6788cdaae03085a0e0d28673fd81b0fe69da290
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-05-17 14:34:46 +0800

    add a venv prompt

--- zsh/custom/myown/aliases.sh
+++ zsh/custom/myown/aliases.sh
@@ -5,3 +5,4 @@ alias vi='vim'
 alias transch='trans :ch'
 alias tmatach='tmux attach-session'
 alias 2lastDir='cd $OLDPWD'
+alias act_venv='source ./bin/activate' # activate virtual env environment under current dir
