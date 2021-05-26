3fe310d104090c57ea0fe9692b039a32ec016f53
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-05-26 09:26:55 +0800

    add a ranger alias

--- zsh/custom/myown/aliases.sh
+++ zsh/custom/myown/aliases.sh
@@ -6,3 +6,4 @@ alias transch='trans :ch'
 alias tmatach='tmux attach-session'
 alias 2lastDir='cd $OLDPWD'
 alias act_venv='source ./bin/activate' # activate virtual env environment under current dir
+alias ranger2='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"' # cd into the last directory ranger has been to when exit ranger
