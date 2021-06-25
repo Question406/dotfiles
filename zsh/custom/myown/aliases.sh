b72b33226c7d4b0ed6db4543d624adaeb2afaf8e
question406 <sjcs_jijiabao@sjtu.edu.cn>
2021-06-25 22:41:40 +0800

    alias add show_gpu

--- zsh/custom/myown/aliases.sh
+++ zsh/custom/myown/aliases.sh
@@ -1,4 +1,3 @@
-alias GPUstatus='nvidia-smi'
 alias whereami='pwd'
 alias clr='clear'
 alias vi='vim'
@@ -9,3 +8,4 @@ alias 2lastDir='cd $OLDPWD'
 alias act_venv='source ./bin/activate' # activate virtual env environment under current dir
 alias ranger2='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"' # cd into the last directory ranger has been to when exit ranger
 alias grep_process='ps -aux | grep grep -v | grep '
+alias show_gpu='nvidia-smi && echo cuda:$CUDA_VISIBLE_DEVICES'
