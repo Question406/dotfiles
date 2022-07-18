ca60601341b8a2848034e5bd5d90d2d3d24012db
question406 <sjcs_jijiabao@sjtu.edu.cn>
2022-07-18 10:32:55 +0800

    update alias

--- zsh/custom/myown/aliases.sh
+++ zsh/custom/myown/aliases.sh
@@ -1,12 +1,12 @@
 alias mv='mv -i'
 alias rm='rm -i'
-alias whereami='pwd'
 alias clr='clear'
 alias vi='vim'
 alias transch='trans :ch'
 alias tmatach='tmux attach-session'
 alias tmlist='tmux list-session'
 alias 2lastDir='cd $OLDPWD'
+alias 2lD='cd $OLDPWD'
 alias act_venv='source ./bin/activate' # activate virtual env environment under current dir
 alias ranger2='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"' # cd into the last directory ranger has been to when exit ranger
 alias grep_process='ps -aux | grep grep -v | grep'
